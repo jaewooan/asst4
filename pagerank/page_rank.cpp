b0bf9f097b43d9fc459d37a173b827dbe1542cc1
jaewooan <jaewooan>
2020-11-01 01:13:40 -0700

    dd

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -25,6 +25,7 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
 
   int numNodes = num_nodes(g);
   double equal_prob = 1.0 / numNodes;
+
   for (int i = 0; i < numNodes; ++i) {
     solution[i] = equal_prob;
   }
@@ -36,26 +37,67 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
      solution may need to allocate (and free) temporary arrays.
 
      Basic page rank pseudocode is provided below to get you started:
+  */
+  // initialization: see example code above
+  
+  int* numEdgesLeavingFrom =  (int*)malloc(sizeof(int) * numNodes); // edge -> node
+  int* numEdgesReachingTo =  (int*)malloc(sizeof(int) * numNodes); // edge -> node
+  double* local_diff = (double*)malloc(sizeof(double) * omp_get_num_threads());
+  double* score_old = (double*)malloc(sizeof(double) * numNodes);
+  int scoreFromNodeNoOutLike = 0;
 
-     // initialization: see example code above
-     score_old[vi] = 1/numNodes;
+  #pragma omp parallel for schedule(guided)
+  for (int vi = 0; vi < numNodes; ++vi) {
+    score_old[vi] = solution[vi];
+    if(i < numNodes - 1){
+      numEdgesLeavingFrom[vi] =  g->outgoing_starts[vi+1] - g->outgoing_starts[vi];
+      numEdgesReachingTo[vi] =  g->incoming_starts[vi+1] - g->incoming_starts[vi];
+    }
+    else{
+      numEdgesLeavingFrom[vi] =  num_edges(g) - g->outgoing_starts[vi];
+      numEdgesReachingTo[vi] =  num_edges(g) - g->incoming_starts[vi];
+    }
+  }
 
-     while (!converged) {
+  bool converged = false;
+  while (!converged) {
+    // initialize for no outgoing edges numEdge    
+    scoreFromNodeNoOutLike = 0;
+    double global_diff = 0;
+    for (int iThread = 0; iThread < omp_get_num_threads(); iThread++){
+      local_diff[iThread] = 0;
+    }
 
-       // compute score_new[vi] for all nodes vi:
-       score_new[vi] = sum over all nodes vj reachable from incoming edges
-                          { score_old[vj] / number of edges leaving vj  }
-       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
+    #pragma omp parallel for schedule(guided)
+    for (int vi = 0; vi < numNodes; ++vi) {
+      // sum over all nodes v in graph with no outgoing edges
+      scoreFromNodeNoOutLike += damping * score_old[vi] / numNodes;
+      solution[vi] = 0;
+    }
+    
+    #pragma omp parallel for schedule(guided)
+    for (int vi = 0; vi < numNodes; ++vi) {
+      for(int iEdge = 0; iEdge < numEdgesReachingTo[vi]; iEdge++) {
+        // compute score_new[vi] for all nodes vi:
+        int vj = graph->incoming_edges[g->incoming_starts[vi] + iEdge];
+        solution[vi] += score_old[vj] / numEdgesLeavingFrom[vj];
+        solution[vi] = (damping * solution[vi]) + (1.0 - damping) / numNodes;
+        solution[vi] += scoreFromNodeNoOutLike;
+      }
+      local_diff[omp_get_thread_num()] += std::fabs(solution[vi] - score_old[vi]);
+    }
 
-       score_new[vi] += sum over all nodes v in graph with no outgoing edges
-                          { damping * score_old[v] / numNodes }
+    for (int iThread = 0; iThread < omp_get_num_threads(); iThread++){
+      global_diff += local_diff[iThread];
+    }
 
-       // compute how much per-node scores have changed
-       // quit once algorithm has converged
+    // compute how much per-node scores have changed
+    // quit once algorithm has converged
+    converged = (global_diff < convergence)
 
-       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
-       converged = (global_diff < convergence)
-     }
-
-   */
+    #pragma omp parallel for schedule(guided)
+    for (int vi = 0; vi < numNodes; ++vi) {
+      score_old[vi] = solution[vi];
+    }
+  }  
 }
