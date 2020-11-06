b0f50b1a50c03320e3a0a611fb3e9ab0e1ddcb13
jaewooan <jaewooan>
2020-11-06 00:28:57 -0800

    bottom top

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -106,6 +106,49 @@ void bfs_top_down(Graph graph, solution* sol) {
     }
 }
 
+// Take one step of "bottom-up" BFS.  For each vertex on the frontier,
+// follow all outgoing edges, and add all neighboring vertices to the
+// new_frontier.
+void bottom_up_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    int* distances)
+{
+    #pragma omp parallel for schedule(guided)
+    for (int node=0; node<g->num_nodes; node++) {
+        if(distances[node] == NOT_VISITED_MARKER){
+            int start_edge = g->incoming_starts[node];
+            int end_edge = (node == g->num_nodes - 1)
+                           ? g->num_edges
+                           : g->incoming_starts[node + 1];
+            int* local_incoming = (int*)malloc(sizeof(int) * (end_edge - start_edge));
+
+            // attempt to add all neighbors to the new frontier
+            int local_count = 0;
+            for (int neighbor=start_edge; neighbor<end_edge; neighbor++) {
+                int incoming = g->incoming_edges[neighbor];
+                for (int i=0; i<frontier->count; i++) {
+                    int frontier_node = frontier->vertices[i];
+                    if (incoming == frontier_node) {
+                        distances[incoming] = distances[frontier_node] + 1;
+                        local_incoming[local_count] = incoming;    
+                        local_count++;	
+                    }
+                }
+            }
+
+            if (local_count > 0){
+                int old_index = __sync_fetch_and_add(&new_frontier->count, local_count);
+                for(int neighbor = 0; neighbor < local_count; neighbor++){
+                    new_frontier->vertices[old_index + neighbor] = local_outgoing[neighbor];
+                }
+            }
+            free(local_incoming);
+        }
+    }
+}
+
 void bfs_bottom_up(Graph graph, solution* sol)
 {
     // CS149 students:
@@ -119,6 +162,44 @@ void bfs_bottom_up(Graph graph, solution* sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+
+    vertex_set list1;
+    vertex_set list2;
+    vertex_set_init(&list1, graph->num_nodes);
+    vertex_set_init(&list2, graph->num_nodes);
+
+    vertex_set* frontier = &list1;
+    vertex_set* new_frontier = &list2;
+
+    // initialize all nodes to NOT_VISITED
+    #pragma omp parallel for schedule(guided)
+    for (int i=0; i<graph->num_nodes; i++)
+        sol->distances[i] = NOT_VISITED_MARKER;
+
+    // setup frontier with the root node
+    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    sol->distances[ROOT_NODE_ID] = 0;
+
+    while (frontier->count != 0) {
+
+#ifdef VERBOSE
+        double start_time = CycleTimer::currentSeconds();
+#endif
+
+        vertex_set_clear(new_frontier);
+
+        bottom_up_step(graph, frontier, new_frontier, sol->distances);
+
+#ifdef VERBOSE
+    double end_time = CycleTimer::currentSeconds();
+    printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+#endif
+
+        // swap pointers
+        vertex_set* tmp = frontier;
+        frontier = new_frontier;
+        new_frontier = tmp;
+    }
 }
 
 void bfs_hybrid(Graph graph, solution* sol)
